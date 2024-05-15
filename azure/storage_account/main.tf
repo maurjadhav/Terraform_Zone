92629f56cb87de67c3af966e4d298c46e9ec0c8e
mayur jadhav <maur.r.jadhav@gmail.com>
2024-05-15 10:57:00 +0530

    storage account

--- azure/storage_account/main.tf
+++ azure/storage_account/main.tf
@@ -2,3 +2,14 @@ resource "azurerm_resource_group" "storage" {
   name     = "storage"
   location = "Central India"
 }
+
+resource "azurerm_storage_account" "example" {
+  name                     = "fromtfmay24"
+  resource_group_name      = azurerm_resource_group.storage.name
+  location                 = azurerm_resource_group.storage.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+  tags = {
+    environment = "staging"
+  }
+}
