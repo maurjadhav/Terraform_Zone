de9a244b35abbde2077b7105de38d4142169a63a
mayur jadhav <maur.r.jadhav@gmail.com>
2024-05-20 17:24:50 +0530

    added another application in script

--- azure/multi-region-network/install.sh
+++ azure/multi-region-network/install.sh
@@ -4,3 +4,7 @@ sudo apt install nginx unzip -y
 cd /tmp && wget https://www.free-css.com/assets/files/free-css-templates/download/page295/kider.zip
 unzip /tmp/kider.zip
 sudo mv /tmp/preschool-website-template /var/www/html/preschool
+
+cd /tmp && wget https://www.free-css.com/assets/files/free-css-templates/download/page296/klinik.zip
+unzip /tmp/klinik.zip
+sudo mv /tmp/clinic-website-template /var/www/html/clinic
