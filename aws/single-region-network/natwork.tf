1b7ea2453b54d93eecfdc68cb8f81b52f822781e
mayur jadhav <maur.r.jadhav@gmail.com>
2024-05-21 14:38:57 +0530

    added internet getway, route table and associate with the subnet

--- aws/single-region-network/natwork.tf
+++ aws/single-region-network/natwork.tf
@@ -34,5 +34,38 @@ resource "aws_subnet" "public" {
 
 }
 
+# internet gatway
+resource "aws_internet_gateway" "internet-getway" {
+  vpc_id = aws_vpc.primary.id
+  tags = {
+    Name = "internet-getway"
+  }
+  depends_on = [aws_vpc.primary]
+
+}
+
+# create public route table
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.primary.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.internet-getway.id
+  }
+
+  depends_on = [aws_vpc.primary, aws_internet_gateway.internet-getway]
+}
+
+
+
+
+# associate public subnets with public route table
+resource "aws_route_table_association" "public" {
+  count          = length(var.public_subnets)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+
+  depends_on = [aws_route_table.public, aws_subnet.public]
+
+}
 
 
