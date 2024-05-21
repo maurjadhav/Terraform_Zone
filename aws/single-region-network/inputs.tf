390cd19dab1b7a116f98e9495dcd33c752d2b4a1
mayur jadhav <maur.r.jadhav@gmail.com>
2024-05-21 17:13:47 +0530

    remove the default values

--- aws/single-region-network/inputs.tf
+++ aws/single-region-network/inputs.tf
@@ -3,11 +3,6 @@ variable "network_info" {
     name       = string
     cidr_block = string
   })
-  default = {
-    name       = "default"
-    cidr_block = "192.168.0.0/16"
-  }
-
 }
 
 variable "private_subnets" {
@@ -16,11 +11,6 @@ variable "private_subnets" {
     cidr_range = string
     av_zone    = string
   }))
-  default = [{
-    name       = "default-db"
-    cidr_range = "192.168.0.0/24"
-    av_zone    = "ap-south-1a"
-  }]
   description = "This is the private subnet"
 }
 
@@ -30,11 +20,6 @@ variable "public_subnets" {
     cidr_range = string
     av_zone    = string
   }))
-  default = [{
-    name       = "default-web"
-    cidr_range = "192.168.1.0/24"
-    av_zone    = "ap-south-1b"
-  }]
   description = "This is the public subnet"
 }
 
