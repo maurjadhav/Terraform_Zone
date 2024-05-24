5fa5d899f9fea40c6ef112eba1ae7b416b18aef2
mayur jadhav <maur.r.jadhav@gmail.com>
2024-05-24 14:25:44 +0530

    added module for security group

--- aws/using_module/main.tf
+++ aws/using_module/main.tf
@@ -1,3 +1,4 @@
+# creating vpc
 module "vpc" {
   source = "../modules/vpc"
   network_info = {
@@ -30,4 +31,51 @@ module "vpc" {
       av_zone    = "ap-south-1b"
   }]
 
+}
+
+# creating security group for web
+module "web_security_group" {
+  source = "../modules/security_group"
+  security_group_info = {
+    name        = "web-sg"
+    description = "web security group"
+    vpc_id      = module.vpc.vpc_id
+    inbound_rules = [{
+      cidr        = "0.0.0.0/0"
+      port        = 22
+      protocol    = "tcp"
+      description = "open ssh"
+      }, {
+      cidr        = "0.0.0.0/0"
+      port        = 80
+      protocol    = "tcp"
+      description = "open http"
+    }]
+    outbound_rules   = []
+    allow_all_egress = true
+  }
+
+  depends_on = [module.vpc]
+}
+
+
+
+# creating security group for db
+module "db_security_group" {
+  source = "../modules/security_group"
+  security_group_info = {
+    name        = "db-sg"
+    description = "db security group"
+    vpc_id      = module.vpc.vpc_id
+    inbound_rules = [{
+      cidr        = "10.10.0.0/16"
+      port        = 3306
+      protocol    = "tcp"
+      description = "open mysql port"
+    }]
+    outbound_rules   = []
+    allow_all_egress = true
+  }
+
+  depends_on = [module.vpc]
 }
