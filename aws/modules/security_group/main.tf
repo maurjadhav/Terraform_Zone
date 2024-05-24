72603c3fe553aa5e826992fe8ad2f3314a8e8034
mayur jadhav <maur.r.jadhav@gmail.com>
2024-05-24 14:29:45 +0530

    added changes

--- aws/modules/security_group/main.tf
+++ aws/modules/security_group/main.tf
@@ -1,47 +1,46 @@
-# security group for web
-resource "aws_security_group" "web" {
+# security group for this
+resource "aws_security_group" "this" {
   name        = var.security_group_info.name
   description = var.security_group_info.description
-  vpc_id      = aws_vpc.primary.id
+  vpc_id      = var.security_group_info.vpc_id
   tags = {
     Name = var.security_group_info.name
   }
-  depends_on = [aws_vpc.primary]
 }
 
 
 # inbound rules
-resource "aws_vpc_security_group_ingress_rule" "web" {
+resource "aws_vpc_security_group_ingress_rule" "this" {
   count             = length(var.security_group_info.inbound_rules)
-  security_group_id = aws_security_group.web.id
+  security_group_id = aws_security_group.this.id
   cidr_ipv4         = var.security_group_info.inbound_rules[count.index].cidr
   from_port         = var.security_group_info.inbound_rules[count.index].port
   to_port           = var.security_group_info.inbound_rules[count.index].port
   ip_protocol       = var.security_group_info.inbound_rules[count.index].protocol
   description       = var.security_group_info.inbound_rules[count.index].description
 
-  depends_on = [aws_security_group.web]
+  depends_on = [aws_security_group.this]
 }
 
 
 # outbound rules
-resource "aws_vpc_security_group_egress_rule" "web" {
+resource "aws_vpc_security_group_egress_rule" "this" {
   count             = length(var.security_group_info.outbound_rules)
-  security_group_id = aws_security_group.web.id
+  security_group_id = aws_security_group.this.id
   cidr_ipv4         = var.security_group_info.outbound_rules[count.index].cidr
   from_port         = var.security_group_info.outbound_rules[count.index].from_port
   to_port           = var.security_group_info.outbound_rules[count.index].to_port
   ip_protocol       = var.security_group_info.outbound_rules[count.index].protocol
   description       = var.security_group_info.outbound_rules[count.index].description
-  depends_on        = [aws_security_group.web]
+  depends_on        = [aws_security_group.this]
 
 }
 
 resource "aws_vpc_security_group_egress_rule" "allow_all_egress" {
   count             = var.security_group_info.allow_all_egress ? 1 : 0
-  security_group_id = aws_security_group.web.id
+  security_group_id = aws_security_group.this.id
   cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = "-1"
-  depends_on        = [aws_security_group.web]
+  depends_on        = [aws_security_group.this]
 
 }
--- aws/modules/security_group/outputs.tf
+++ aws/modules/security_group/outputs.tf
@@ -1,12 +1,12 @@
 output "aws_security_group" {
-  value = aws_security_group.web
+  value = aws_security_group.this
 }
 
 output "aws_vpc_security_group_ingress_rule" {
-  value = aws_vpc_security_group_ingress_rule.web
+  value = aws_vpc_security_group_ingress_rule.this
 }
 
 output "aws_vpc_security_group_egress_rule" {
-    value = aws_vpc_security_group_egress_rule.web
+    value = aws_vpc_security_group_egress_rule.this
   
 }
