cc8f04763543b1b458072dac0d341621a73f14eb
mayur jadhav <maur.r.jadhav@gmail.com>
2024-05-30 16:20:10 +0530

    remove the scale set

--- azure/sample-lb/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "url" {
-  value = azurerm_lb.this
-}
-
-output "public_ip" {
-  value = azurerm_public_ip.this.ip_address
-}
--- azure/sample-lb/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "subnet_name" {
-  type    = list(string)
-  default = ["web", "app", "app"]
-}
-
-variable "subnet_cidr" {
-  type    = list(string)
-  default = ["10.10.0.0/24", "10.10.1.0/24", "10.10.2.0/24"]
-}
